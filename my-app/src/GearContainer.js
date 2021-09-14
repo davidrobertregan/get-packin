02d06c1a2bb9620658ed1b75f0a0f9d1cdcc550e
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-14 08:07:25 -0500

    added component files

---
+++ my-app/src/GearContainer.js
@@ -0,0 +1,10 @@
+
+function GearContainer() {
+    return (
+        <div>
+            <h1>Gear Container</h1>
+        </div>
+    )
+}
+
+export default GearContainer
---
+++ my-app/src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div>
+            <h1>Get Packin'</h1>
+            <p>A backpacker's interactive packing list</p>
+        </div>
+    )
+}
+
+export default Header

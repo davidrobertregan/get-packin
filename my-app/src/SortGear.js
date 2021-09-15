22aa0c061994657bb4e861cc3186290c5aaf7c54
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-15 14:23:12 -0500

    styled sort buttons

--- my-app/src/SortGear.js
+++ my-app/src/SortGear.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Button, ButtonGroup} from 'react-bootstrap'
 
 function SortGear({ filterCategory }) {
 
@@ -6,15 +7,18 @@ function SortGear({ filterCategory }) {
         filterCategory(e)
     }
 
+
     return (
         <>
         <h3>Sort By:</h3>
-          <button onClick={handleClick} name='All Items' >All Items</button>
-          <button onClick={handleClick} name='Clothing' >Clothing</button>
-          <button onClick={handleClick} name='Packs & Bags'>Packs & Bags</button>
-          <button onClick={handleClick} name='Sleeping Gear'>Sleeping Gear </button>
-          <button onClick={handleClick} name='Miscellaneous Items'>Miscellaneous Items</button>
-          <button onClick={handleClick} name='Optional Items'>Optional Items</button>
+          <ButtonGroup size="md">
+            <Button variant="outline-secondary" onClick={handleClick} name='All Items' >All Items</Button>
+            <Button variant="outline-secondary" onClick={handleClick} name='Clothing' >Clothing</Button>
+            <Button variant="outline-secondary" onClick={handleClick} name='Packs & Bags'>Packs & Bags</Button>
+            <Button variant="outline-secondary" onClick={handleClick} name='Sleeping Gear'>Sleeping Gear </Button>
+            <Button variant="outline-secondary" onClick={handleClick} name='Miscellaneous Items'>Miscellaneous Items</Button>
+            <Button variant="outline-secondary" onClick={handleClick} name='Optional Items'>Optional Items</Button>
+          </ButtonGroup>
           </>
     )
 }

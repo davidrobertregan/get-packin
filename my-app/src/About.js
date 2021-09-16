7015891b052c639911908e2edfe70cd8333a4b03
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-16 15:20:56 -0500

    about styling

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -2,17 +2,19 @@ import {Container, Figure} from "react-bootstrap"
 
 function About () {
     return (
-        <Container className="p-5">
+        <Container className="p-3">
             <div>
                 <h1>⛺️Get Packin'</h1>
-                <h3>"Where's the stove?"</h3>
-                <div>
-                    <p>Nobody likes to realize they forgot something long after they hit the trail. But it's also easy to do... there's so many things to pack and all the gear is usually living on multiple separate lists. Thus the idea for Get Packin' was born: one place for all your gear so you'll never forget anything again.</p>
-                </div>
+                <Container className='p-5'>
+                    <h3>"Where's the stove?"</h3>
+                    <div>
+                        <p>Nobody likes to realize they forgot something long after they hit the trail. But it's also easy to do! There are so many things to pack and all the gear is usually living on separate lists. Thus the idea for Get Packin' was born: one interactive app for all your gear so you'll never forget anything again! (We make no guarantees)</p>
+                    </div>
+                </Container>
             </div>
-            <div>
+            <Container bg='primary' className='p-1'>
                 <h3>Meet the Creators</h3>
-                <Container>
+                <div>
                     <Figure>
                         <Figure.Image
                             src="https://static.wixstatic.com/media/065e6b_dd1a8624bd5b40c9848aeec671ed811b.png/v1/fit/w_936%2Ch_733%2Cal_c/file.png"
@@ -23,8 +25,8 @@ function About () {
                             thumbnail
                             width={210}/>
                     </Figure>
-                </Container>
-            </div>
+                </div>
+            </Container>
         </Container>
     )
 }
